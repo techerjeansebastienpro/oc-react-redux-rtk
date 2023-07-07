a48de48543a70ccb00053d05498db5232deb3b71
jean sébastien techer <techerjeansebastien.pro@gmail.com>
2023-07-07 16:17:34 +0200

    p2c2

--- src/app/App.js
+++ src/app/App.js
@@ -2,12 +2,16 @@ import { Provider } from 'react-redux'
 import { store } from './store';
 import './App.css';
 import { Cart } from '../features/cart/Cart';
+import { Total } from '../features/total/Total';
+import { Voucher } from '../features/voucher/Voucher';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
         <Cart />
+        <Total />
+        <Voucher />
       </div>
     </Provider>
   );
