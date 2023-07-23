370ab71b27e6619f24b23cb1f57fbebc7a0638cd
jean sébastien techer <techerjeansebastien.pro@gmail.com>
2023-07-23 11:24:24 +0200

    p3c1-solution

--- src/features/menu/Menu.js
+++ src/features/menu/Menu.js
@@ -1,12 +1,13 @@
 import { useDispatch } from 'react-redux';
 import * as ProductList from '../../common/models';
 import { ProductCard } from '../../common/components/ProductCard';
+import { addProduct } from '../../app/store';
 
 export const Menu = () => {
     const dispatch = useDispatch();
     return <div className="Menu">
         {
-            Object.values(ProductList).map(product => <ProductCard key={product.name} product={product} onSelect={() => dispatch({ type: 'ADD_PRODUCT', payload: product })} />)
+            Object.values(ProductList).map(product => <ProductCard key={product.name} product={product} onSelect={() => dispatch(addProduct(product))} />)
         }
     </div>
 }
